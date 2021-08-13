38a6b7985ddd8f6c47cd5e9ec45affbd828a4a02
Olga-Shuliak <wirasta@gmail.com>
2021-08-13 23:55:28 +0300

    init: task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const toNineteen = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const toNinety = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+    const hundred = 'hundred';
+    if (number === 0) return 'zero';
+    if (number < 20) return toNineteen[number];
+    if (number >= 20 && number < 100) {
+        let decades = Math.floor(number / 10);
+        let units = number % 10;
+        return (`${toNinety[decades]}${toNineteen[units]}`).trim()
+    }
+    if (number >= 100 && number <= 999) {
+        let hundreds = Math.floor(number / 100);
+        let decades = Math.floor((number - hundreds * 100) / 10);
+        let units = number % 10;
+        if( number % 100 <= 19 ){
+            units = number % 100
+        }
+        return (`${toNineteen[hundreds]} ${hundred} ${toNinety[decades]}${toNineteen[units]}`).trim()
+    }
 }
